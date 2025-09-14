a9b0f622b6387e17fe024b83c0ffdf0e1d96ffb3
Jacob Strauss <jacob.d.strauss@gmail.com>
2025-09-14 14:38:48 +0100

    Add CORS support to the main router configuration

--- src/main.go
+++ src/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jda5/table-tennis/src/internal/handlers"
 	"github.com/jda5/table-tennis/src/internal/stores"
+
+	"github.com/gin-contrib/cors"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(
+		cors.New(
+			cors.Config{
+				AllowOrigins:     []string{"http://localhost:3000", "https://luincpong.com"},
+				AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+				AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+				AllowCredentials: true,
+				MaxAge:           12 * time.Hour,
+			},
+		),
+	)
 	h := handlers.APIHandler{Store: stores.CreateMySQLDAO()}
 
 	router.GET("/leaderboards", h.GetLeaderboard)
